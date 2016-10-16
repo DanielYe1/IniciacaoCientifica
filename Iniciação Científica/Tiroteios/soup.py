404a0d30906398ee62e851660ee0ba5d591b62bb
Daniel Ye <danielxinxin1@gmail.com>
2016-10-16 13:17:46 -0200

    Implemented shooting cases in Rio with lat/long

--- Iniciação Científica/Tiroteios/soup.py
+++ Iniciação Científica/Tiroteios/soup.py
@@ -1,26 +1,32 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-dadosDesnecessarios = ["Mortos", "Civis", "Policiais", "Feridos", "Operação Policial", "Fonte", "Usuário"]
+dadosDesnecessarios = ["Mortos", "Civis", "Policiais", "Feridos", "Operação Policial", "Fonte", "Usuário", "Imprensa"]
+
 nomeArq = "dadosFogoCruzado.csv"
-bd = open(nomeArq, "w")
-bd.write(
-    "LOCAL DO TIROTEIO,DATA DO TIROTEIO,HORÁRIO DO TIROTEIO,MORTES?,CIVIS MORTOS,POLICIAIS MORTOS,FERIDOS?,CIVIS FERIDOS,POLICIAIS FERIDOS,OPERAÇÃO POLICIAL?\n")
-bd.close()
-for i in range(1, 1000):
+with open(nomeArq, 'w', newline='') as csvfile:
+    csvfile.write(
+        "LOCAL DO TIROTEIO,DATA DO TIROTEIO,HORÁRIO DO TIROTEIO,MORTES?,CIVIS MORTOS,POLICIAIS MORTOS,FERIDOS?,CIVIS FERIDOS,POLICIAIS FERIDOS,OPERAÇÃO POLICIAL?,LATITUDE,LONGITUDE,INDEX\n")
+for i in range(0, 3500):
     try:
         r = requests.get("http://fogocruzado.org.br/tiroteio/" + str(i) + "/", auth=('user', 'pass'))
-        teste = BeautifulSoup(r.text, "html.parser")
+        data = BeautifulSoup(r.text, "html.parser")
 
-        valor = teste.find_all("div", class_="wpb_text_column wpb_content_element ")
+        valor = data.find_all("div", class_="wpb_text_column wpb_content_element ")
 
         dados = ""
         for val in valor:
             x = val.get_text().strip().replace(",", "|")
             if x not in dadosDesnecessarios:
                 dados += x + ","
+
+        location = re.findall('de_mapa_atual = (.*);', str(data))
+        for x in location:
+            dados += str(x) + ","
+
         if dados != "":
-            dados = dados.strip(",") + "\n"
+            dados = dados.strip(",") + "," + str(i) + "\n"
             bd = open(nomeArq, "a")
             bd.write(dados)
             bd.close()
